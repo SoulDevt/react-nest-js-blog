acc9fc3c49711eac96c953d203c6349922e87e03
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-05-30 23:50:35 +0200

    add entity and getuser getsingle user

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+// import { UsersController } from './users/users.controller';
+// import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entity/users.entity';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'mauFJcuf5dhRMQrjj',
+    database: 'react-blog-app',
+    entities: [User],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/users.entity';
 
 @Injectable()
 export class UsersService {
-    getUsers() {
-        return {id : 1, name: 'John', email: 'john@gmail.com'}
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+      ) {}
+    getUsers(): Promise<User[]> {
+        return this.usersRepository.find();
     }
-    getSingleUser(param: number) {
-        return param;
+    getSingleUser(id: number): Promise<User | null> {
+        return this.usersRepository.findOneBy({ id });
     }
     postUser(createUserDto: CreateUserDto) {
         return createUserDto;
     }
-    deleteUser(param: {id: number}) {
-        return {param};
+    async deleteUser(id: {id: number}) {
+        await this.usersRepository.delete(id);
     }
     editUser(updateUserDto: UpdateUserDto, param: {id: number}) {
         return {body: updateUserDto, param};
